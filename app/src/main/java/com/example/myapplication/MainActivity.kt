696339230e65db85eab17aaf530148e0236f48b1
SUSHANT CHAUHAN <sushantchauhan08@gmail.com>
2024-04-21 15:15:25 +0530

    Second commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -38,6 +38,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     MyApplicationTheme {
-        Greeting("Android")
+        Greeting("Sushant")
     }
 }
